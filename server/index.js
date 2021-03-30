28ebcd0836c9d59eb3609ba8f840490c33af5ac9
Harsh Verma <harshverma0362@gmail.com>
2021-03-31 00:32:55 +0530

    edited code of index file in backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import errorHandling from "./middlewares/errorModdleware.js";
 import UserRoutes from "./routes/userRoutes.js";
+import path from 'path';
 
 dotenv.config();
 
@@ -12,9 +13,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API is running..."));
-
 app.use("/api/users", UserRoutes);
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, '/client/build')))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+	})
+} else {
+	app.get("/", (req, res) => res.send("API is running..."));
+}
 
 app.use(errorHandling);
 
