79877d5dab0c4c3f7522a532e6691f5a253b5091
Harsh Verma <harshverma0362@gmail.com>
2021-03-31 21:27:58 +0530

    error fixed

--- server/index.js
+++ server/index.js
@@ -30,4 +30,4 @@ if (process.env.NODE_ENV === "production") {
 app.use(errorHandling);
 
 // const PORT = 5000 || process.env.PORT;
-app.listen(process.env.PORT || 5000, console.log(`Server is running on PORT ${PORT}`));
+app.listen(process.env.PORT || 5000, console.log(`Server is running on PORT ${process.env.PORT}`));
